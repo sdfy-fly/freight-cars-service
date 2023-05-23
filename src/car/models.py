87b0c27cbc17a20883da9d7fabcd128669d6ac73
sdfy-fly <rozhov25@bk.ru>
2023-05-23 18:43:56 +0300

    change models

--- src/car/models.py
+++ src/car/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.db.models import PROTECT
 
@@ -7,4 +8,4 @@ from src.location.models import Location
 class Car(models.Model):
     unique_number = models.CharField(max_length=5, unique=True)
     current_location = models.ForeignKey(Location, on_delete=PROTECT)
-    capacity = models.PositiveIntegerField()
+    capacity = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(1000)])
--- src/cargo/models.py
+++ src/cargo/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.db.models import CASCADE
 
@@ -5,8 +6,7 @@ from src.location.models import Location
 
 
 class Cargo(models.Model):
-
-    pick_up_location = models.ForeignKey(Location, on_delete=CASCADE)
-    delivery_location = models.ForeignKey(Location, on_delete=CASCADE)
-    weight = models.PositiveIntegerField()
+    pick_up_location = models.ForeignKey(Location, on_delete=CASCADE, related_name='pick_up_cargos')
+    delivery_location = models.ForeignKey(Location, on_delete=CASCADE, related_name='delivery_cargos')
+    weight = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(1000)])
     description = models.TextField()
